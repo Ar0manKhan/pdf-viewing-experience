2b39cc87a7d680fa2a06833c0943b64f9061086d
Arman Khan <arman@klimb.io>
2025-09-15 06:07:00 +0530

    feat: rendering all the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import PdfCanvas from "./PdfCanvas";
-import TTSWeb from "./TTSWeb";
 
 function App() {
   return (
-    <div className="w-screen h-screen">
-      <TTSWeb />
+    <div className="w-screen h-screen overflow-auto">
+      {/*<TTSWeb />*/}
       <PdfCanvas />
     </div>
   );
--- src/PdfCanvas.tsx
+++ src/PdfCanvas.tsx
@@ -3,52 +3,34 @@ import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import { useCallback, useState } from "react";
 import PdfPage from "./PdfPage";
+import type { OnDocumentLoadSuccess } from "react-pdf/dist/shared/types.js";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const url = "/b-tree.pdf";
 
 export default function PdfCanvas() {
-  const [numPages, setNumPages] = useState<number>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [pageCounts, setPageCounts] = useState<number[]>([]);
+  const [pageDim, setPageDim] = useState({ height: 0, width: 0 });
 
-  const onDocumentLoadSuccess = useCallback(function ({
-    numPages,
-  }: {
-    numPages: number;
-  }) {
-    setNumPages(numPages);
-  }, []);
-
+  const onDocumentLoadSuccess: OnDocumentLoadSuccess = useCallback(
+    async ({ numPages }) => {
+      // take a middle page for page dimension
+      setPageCounts(Array.from({ length: numPages }, (_, i) => i + 1));
+    },
+    [],
+  );
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-green-300">
+    <div className="flex flex-col items-center justify-center bg-green-300">
+      {/*<p>
+        {pageDim.height} {pageDim.width}
+      </p>*/}
       <div className="w-4/5 overflow-scroll relative">
-        <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
-          <PdfPage pageNumber={pageNumber} />
+        <Document file={url} onLoadSuccess={onDocumentLoadSuccess} scale={2}>
+          {pageCounts.map((pageNumber) => (
+            <PdfPage key={pageNumber} pageNumber={pageNumber} />
+          ))}
         </Document>
-      </div>
-      <div className="flex items-center justify-center gap-4 p-4">
-        <button
-          onClick={() => {
-            setPageNumber(Math.max(1, pageNumber - 1));
-          }}
-          disabled={pageNumber <= 1}
-          className="px-4 py-2 bg-gray-800 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 disabled:bg-gray-400 disabled:cursor-not-allowed transition-colors"
-        >
-          Previous
-        </button>
-        <p className="text-lg font-semibold text-gray-800">
-          Page {pageNumber} of {numPages || "--"}
-        </p>
-        <button
-          onClick={() => {
-            setPageNumber(Math.min(numPages || 1, pageNumber + 1));
-          }}
-          disabled={pageNumber >= (numPages || 1)}
-          className="px-4 py-2 bg-gray-800 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 disabled:bg-gray-400 disabled:cursor-not-allowed transition-colors"
-        >
-          Next
-        </button>
       </div>
     </div>
   );
