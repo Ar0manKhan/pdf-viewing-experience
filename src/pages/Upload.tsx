5407cdf8d1ef8a9c33234910a9d2852249a5551a
Arman Khan <73093255+Ar0manKhan@users.noreply.github.com>
2025-09-15 06:07:00 +0530

    feat: basic setup for file upload

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -1,3 +1,29 @@
+import {
+  Dropzone,
+  DropzoneEmptyState,
+  DropzoneContent,
+} from "@/components/ui/shadcn-io/dropzone";
+import { useState } from "react";
+
 export default function Upload() {
-  return <div>Upload</div>;
+  const [files, setFiles] = useState<File[] | undefined>();
+
+  const handleDrop = (files: File[]) => {
+    console.log(files);
+    setFiles(files);
+  };
+
+  return (
+    <Dropzone
+      accept={{ "application/pdf": [] }}
+      maxFiles={1}
+      maxSize={1024 * 1024 * 200}
+      onDrop={handleDrop}
+      onError={console.error}
+      src={files}
+    >
+      <DropzoneEmptyState />
+      <DropzoneContent />
+    </Dropzone>
+  );
 }
