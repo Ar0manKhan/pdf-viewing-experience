818e7d7569f1a510e063099f77e49dc5aea34954
Arman Khan <arman@klimb.io>
2025-09-15 06:07:00 +0530

    feat: working tts in chrome and firefox

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import PdfCanvas from "./PdfCanvas";
+import TTSWeb from "./TTSWeb";
 
 function App() {
   return (
     <div className="w-screen h-screen">
+      <TTSWeb />
       <PdfCanvas />
     </div>
   );
